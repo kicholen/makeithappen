c60f948a41f609ddc5b165098ac24c2e5b0fd215
Mateusz Debski <mateusz.debski@picadilla.com>
2018-05-19 22:07:26 +0200

    Mail sender

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -1,6 +1,28 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-	    statusCode: 200,
-	    body: process.env.LOGIN
+    var nodemailer = require('nodemailer');
+    var smtpTransport = require('nodemailer-smtp-transport');
+
+    var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.MAIL_LOGIN,
+            pass: process.env.MAIL_PASS
+        }
+    }));
+
+    transporter.sendMail({
+        from: process.env.MAIL_LOGIN,
+        to: process.env.MAIL_TO,
+        subject: process.env.SUBJECT,
+        text: event.body
+    }, function(error, info) {
+    	if (error) {
+    		callback(error);
+    	} else {
+    		callback(null, {
+			    statusCode: 200,
+			    body: "Mail sent! " + info.response
+	    	});
+    	}
     });
 }
