3f7b38548b21d10cbff9c4b2c025fbbbac6a57ac
Mateusz Debski <mateusz.debski@picadilla.com>
2018-05-20 18:39:14 +0200

    Form completed

--- public/js/jelly.js
+++ public/js/jelly.js
@@ -1,4 +1,7 @@
 (function($) {
+    var cacheMap = {};
+    var current;
+
     "use strict";
 
     $('#makeitNav').affix({
@@ -45,29 +48,127 @@
             height: window.innerHeight > parseInt(galleryOptions.maxHeight) ? galleryOptions.maxHeight : galleryOptions.height
           });
       });
-    });
-    var dateTimeOptions = {
 
-    }
-    $(function () {
-      $('#datetimepicker6').datetimepicker({
-        format: 'DD/MM/YYYY',
-        minDate: moment()
-      });
-      $('#datetimepicker7').datetimepicker({
-        format: 'DD/MM/YYYY',
-        useCurrent: false //Important! See issue #1075
+      $(function () {
+        $('#product').on('change', function(e) {
+            $('#datetimepicker_from').data("DateTimePicker").clear();
+            $('#datetimepicker_to').data("DateTimePicker").clear();
+            var url = this.options[this.selectedIndex].getAttribute("data") + "index.json";
+            console.log(url);
+            current = url;
+            if (cacheMap[url])
+            {
+              $('#datetimepicker_from').data("DateTimePicker").disabledDates(cacheMap[url]);
+              $('#datetimepicker_to').data("DateTimePicker").disabledDates(cacheMap[url]);
+            }
+            else
+            {
+              $.ajax({
+                type: 'GET',
+                url: url,
+                dataType: 'json'
+              }).done(function (response) {
+                $.each(response, function (k, v) {
+                  response[k] = moment(response[k]);
+                });
+                cacheMap[url] = response;
+                $('#datetimepicker_from').data("DateTimePicker").disabledDates(response);
+                $('#datetimepicker_to').data("DateTimePicker").disabledDates(response);
+              });
+            }
+        });
+
+        $('#datetimepicker_from').datetimepicker({
+          format: 'DD/MM/YYYY',
+          minDate: moment()
+        });
+
+        $('#datetimepicker_to').datetimepicker({
+          format: 'DD/MM/YYYY',
+          minDate: moment(),
+          useCurrent: false //Important! See issue #1075
+        });
+
+        $("#datetimepicker_from").on("dp.change", function (e) {
+            $('#datetimepicker_to').data("DateTimePicker").clear();
+            if (cacheMap[current]) 
+            {
+              var list = cacheMap[current];
+              var min = null;
+              for (var val in list) {
+                if (list[val] > e.date && (min == null || list[val] < min))
+                  min = list[val];
+              }
+              if (min)
+                $('#datetimepicker_to').data("DateTimePicker").maxDate(min);
+            }
+            $('#datetimepicker_to').data("DateTimePicker").minDate(e.date);
+        });
       });
 
-      $("#datetimepicker6").on("dp.show", function (e) {
-          $('#datetimepicker6').data("DateTimePicker").clear();
-          $('#datetimepicker7').data("DateTimePicker").clear();
+      function validate(object, isCorrect) {
+        if (isCorrect)
+          object.removeClass("has-error").addClass("valid");
+        else
+          object.removeClass("valid").addClass("has-error");
+      }
+
+      $('#contact-fullname').on('input', function() {
+        validate($(this), $(this).val());
       });
-      $("#datetimepicker6").on("dp.change", function (e) {
-          $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+
+      $('#contact-email').on('input', function() {
+        var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        validate($(this), regex.test($(this).val()));
       });
-      $("#datetimepicker7").on("dp.change", function (e) {
-          $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+
+      $('#contact-form').on('submit', function(e) {
+        e.preventDefault();
+        var contactData = $(this).serializeArray();
+        for (var index in contactData) {
+          var element = $("#contact-" + contactData[index]['name']);
+          console.log(element);
+          var valid = element.hasClass("valid");
+          var error_element = $("span", element.parent());
+        }
+        /*var messageAlert = 'alert-' + data.type;
+        var messageText = data.message;
+        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+        
+        if (messageAlert && messageText) {
+          $('#contact-form').find('.messages').html(alertBox);
+          $('#contact-form')[0].reset();
+        }*/
+        var elements = $(this)[0].elements;
+        for (var index in elements) {
+          console.log(elements[index].value);
+        }
+        /*$.ajax({
+            type: "POST",
+            url: "https://thirsty-euler-c41f2b.netlify.com/.netlify/functions/sendMail",
+            data: $(this).serialize(),
+            success: function(data, text, xhr) {
+              console.log(data);
+              console.log(text);
+              console.log(xhr);
+
+              var messageAlert = 'alert-' + data.type;
+              var messageText = data.message;
+              var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+              
+              if (messageAlert && messageText) {
+                $('#contact-form').find('.messages').html(alertBox);
+                $('#contact-form')[0].reset();
+              }
+            },
+            error: function(data, text, error) {
+              console.log(data);
+              console.log(text);
+              console.log(error);
+              $('#contact-form')[0].reset();
+            }
+          });*/
+        return false;
       });
     });
 })(jQuery);
--- themes/jel/static/js/jelly.js
+++ themes/jel/static/js/jelly.js
@@ -49,6 +49,13 @@
           });
       });
 
+      function validate(object, isCorrect) {
+        if (isCorrect)
+          object.parent().removeClass("has-error").addClass("valid");
+        else
+          object.parent().removeClass("valid").addClass("has-error");
+      }
+
       $(function () {
         $('#product').on('change', function(e) {
             $('#datetimepicker_from').data("DateTimePicker").clear();
@@ -90,6 +97,7 @@
         });
 
         $("#datetimepicker_from").on("dp.change", function (e) {
+            validate($(this), true);
             $('#datetimepicker_to').data("DateTimePicker").clear();
             if (cacheMap[current]) 
             {
@@ -99,12 +107,94 @@
                 if (list[val] > e.date && (min == null || list[val] < min))
                   min = list[val];
               }
-              console.log(min);
               if (min)
                 $('#datetimepicker_to').data("DateTimePicker").maxDate(min);
             }
             $('#datetimepicker_to').data("DateTimePicker").minDate(e.date);
         });
+      });
+
+      $('#contact-fullname').on('input', function() {
+        validate($(this), true);
+      });
+
+      $('#contact-email').on('input', function() {
+        validate($(this), true);
+      });
+
+      $('#contact-product').on('change', function() {
+        validate($(this), true);
+      });
+
+      $('#contact-phone').on('input', function() {
+        validate($(this), true);
+      });
+
+      $('#datetimepicker_to').on('dp.change', function() {
+        validate($(this), true);
+      });
+
+      $('#contact-form').on('submit', function(e) {
+        e.preventDefault();
+
+        // validate 
+        var isFullNameValid = $('#contact-fullname').val();
+        validate($('#contact-fullname'), isFullNameValid);
+
+        var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var isEmailValid = emailRegex.test($('#contact-email').val());
+        validate($('#contact-email'), isEmailValid);
+
+        var isProductValid = $('#contact-product').val() !== "Wybierz produkt";
+        validate($('#contact-product'), isProductValid);
+
+        validate($('#contact-phone'), $('#contact-phone').val());
+
+        var isFromDateValid = $('#datetimepicker_from').data("DateTimePicker").date();
+        validate($('#datetimepicker_from'), isFromDateValid);
+
+        var isToDateValid = $('#datetimepicker_to').data("DateTimePicker").date();
+        validate($('#datetimepicker_to'), isToDateValid);
+
+        if (!isFullNameValid || !isEmailValid || !isProductValid || !isFromDateValid || !isToDateValid)
+          return;
+        var dataToSend = "Imię i nazwisko: " + isFullNameValid + '\n'
+          + "Email: " + $('#contact-email').val()  + '\n'
+          + "Product: " + $('#contact-product').val()  + '\n'
+          + "Telefon: " + $('#contact-phone').val()  + '\n'
+          + "Data od: " + isFromDateValid.format("MM/DD/YYYY") + " do: " + isToDateValid.format("MM/DD/YYYY") + '\n'
+          + "Ilość sztuk: " + $('#contact-count').val()  + '\n'
+          + "Tekst: " + $('#contact-question').val();
+
+        // send 
+        $.ajax({
+            type: "POST",
+            url: ".netlify/functions/sendMail",
+            data: $(this).serialize(),
+            success: function(data, text, xhr) {
+              console.log(data);
+              console.log(text);
+              console.log(xhr);
+
+              var alertBox = '<div id="jelly-alert" class="alert alert-success alert-dismissable alert-fixed"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Wysłano!</div>';
+              $('#contact-form').find('.messages').html(alertBox);
+              $("#jelly-alert").delay(3000).slideUp(200, function() {
+                $(this).alert('close');
+              });
+              $('#contact-form')[0].reset();
+            },
+            error: function(data, text, error) {
+              console.log(data);
+              console.log(text);
+              console.log(error);
+              var alertBox = '<div id="jelly-alert" class="alert alert-warning alert-dismissable alert-fixed"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Oops, wystąpił błąd, przepraszamy.</div>';
+              $('#messages').html(alertBox);
+              $("#jelly-alert").delay(3000).slideUp(200, function() {
+                $(this).alert('close');
+              });
+            }
+        });
+        return false;
       });
     });
 })(jQuery);
